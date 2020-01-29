d1629b8cdadc7b6bba59868d2a8a4a9c98b982b5
NBKelly <nbk4@students.waikato.ac.nz>
2020-01-29 15:14:58 +1300

    Faction influence works

--- src/builder/Decklists.java
+++ src/builder/Decklists.java
@@ -12,6 +12,22 @@ import java.net.*;
 import org.apache.commons.io.IOUtils;
 
 public class Decklists {
+    private class FactionList {
+	List<Faction> data;
+	public int total;
+	public boolean success;
+	public String version_number;
+	public String last_updated;
+    }
+
+    private class Faction {
+	String code;
+	String color;
+	boolean is_mini;
+	String name;
+	String side_code;
+    }
+
     private class MWL {
 	public int id;
 	public String data_creation;
@@ -105,8 +121,12 @@ public class Decklists {
 	}
     }
 
+    private static TreeMap<String, Faction> factions = new TreeMap<String, Faction>();
     private static TreeMap<String, String> cards = new TreeMap<String, String>();
     private static TreeMap<String, String> cardCodes = new TreeMap<String, String>();
+    public static TreeMap<String, String> cardFaction = new TreeMap<String, String>();
+    public static TreeMap<String, Integer> cardInfluence = new TreeMap<String, Integer>();
+    
     private static String decklistPrefix = "latexGeneratedDecklist";
     private static String decknamePrefix = "latexGeneratedDeckname";
     private static StringBuilder flush = new StringBuilder();
@@ -116,6 +136,8 @@ public class Decklists {
     private static String MWLVersion = null;
     private static boolean regeneratedLibrary = false;
     private static MWL chosenMWL = null;
+
+    private static boolean processFactionInfluence = false;
     
     public static void main(String[] args) {
 	//each argument is expected to be a decklist file
@@ -133,6 +155,12 @@ public class Decklists {
 		System.err.println("%Downloading standard cardpool (-r option used)");
 		download_library();
 	    }
+
+	    else if (s.equals("-f")) {
+		System.err.println("%Processing faction influence for these cards");
+		processFactionInfluence = true;
+		//we should be able to regenerate the factions file from the api, if need be
+	    }
 	}
 	
 	process_preamble();
@@ -140,16 +168,22 @@ public class Decklists {
 	process_netrunner_library();
 
 	if(useMWL) {
+	    boolean orgl = regeneratedLibrary;
 	    regeneratedLibrary = false;
 	    process_mwl_version();
 	    System.err.println("%Using mwl version: " + MWLVersion);
-
+	    
 	    process_netrunner_mwl();
+	    regeneratedLibrary = orgl;
+	}
+
+	if (processFactionInfluence) {
+	    process_faction_list();
 	}
 	    
 	
 	for(String s : args) {
-	    if(s.equals("-mwl") || s.equals("-r"))
+	    if(s.equals("-mwl") || s.equals("-r") || s.equals("-f"))
 		continue;
 	    process_decklist(s);
 	    deckNum++;
@@ -160,6 +194,44 @@ public class Decklists {
 	System.out.println("\\end{document}");
     }
 
+    private static void process_faction_list() {
+	String fname = "conf/NRDB_factions.json";
+	String factionFile = read_factions_list(fname);
+
+	boolean valid = factionFile != null;
+	System.out.println("%Faction List Read: " + valid);
+
+	Gson gson = new Gson();
+	FactionList fl = gson.fromJson(factionFile, FactionList.class);
+
+	for(Faction f : fl.data) {
+	    factions.put(f.code, f);
+	}
+
+	System.err.println("%Number of factions known: " + factions.size());
+    }
+    
+    private static String read_factions_list(String fname /*NRDB_factions.json*/){
+	StringBuilder builder = new StringBuilder();
+	try (Stream<String> stream = Files.lines(Paths.get(fname), StandardCharsets.UTF_8)) {
+	    stream.forEach(s -> builder.append(s).append("\n"));
+	} catch (IOException e) {
+	    //the file doesn't exist: have we tried fetching it yet this execution?
+	    if(!regeneratedLibrary) {
+		System.err.println("% couldn't open factions file. Checking online...");
+		if(download_factions())
+		    return read_factions_list(fname);
+		else
+		    System.err.println("% Could not read factions list");
+	    }
+	    
+	    //e.printStackTrace();
+	    return null;
+	}
+	
+	return builder.toString();
+    }
+    
     private static void process_mwl_version() {
 	String fname = "conf/mwl.conf";
 	List<String> mwlFile = read_mwl_version(fname);
@@ -204,6 +276,8 @@ public class Decklists {
 	    //put in cards (name, type)
 	    cards.put(c.title, c.type_code);
 	    cardCodes.put(c.code, c.title);
+	    cardFaction.put(c.title, c.faction_code);
+	    cardInfluence.put(c.title, c.faction_cost);
 	}
 
 	System.out.println("%" + library.data.size());	
@@ -292,6 +366,25 @@ public class Decklists {
 	return builder.toString();
     }
 
+    private static boolean download_factions() {
+	regeneratedLibrary = true; //we only make one attempt at this per run
+	//TODO: put these urls in a conf file
+	try {
+	    URL nrdb = new URL("https://netrunnerdb.com/api/2.0/public/factions");
+	    
+	    File targetFile = new File("conf/NRDB_factions.json");
+	    InputStream stream = nrdb.openStream();
+	    java.nio.file.Files.copy(stream, targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+	    
+	    IOUtils.closeQuietly(stream);
+	    return true;
+	} catch (Exception e) {
+	    //System.err.println(e.toString());
+	    System.err.println("%Cannot download netrunner factions list. Please check your internet connection");
+	    return false;
+	}
+    }
+    
     private static boolean download_library() {
 	regeneratedLibrary = true; //we only make one attempt at this per run
 	//TODO: put these urls in a conf file
@@ -344,21 +437,39 @@ public class Decklists {
 	}
 
 	
-	String first = decklist.get(0);
+	
 	String deckName = s;
 	String idName = "Unspecified ID";
 
-	//see if the first line is a deck-name. If it is, we use that.
-	if(first.length() > 1 && first.charAt(0) == '_') {
-	    deckName = first.substring(1);
-	    decklist.remove(0);
-	}
+	String chosenFaction = "auto";
+	boolean facPassed = false;
+	
+	while(true) {
+	    String first = decklist.get(0);
 
-	first = decklist.get(0);
-	//see if the (new) first line is an identity. If it is, we use that.
-	if(first.length() > 1 && first.charAt(0) == '#') {
-	    idName = first.substring(1);
-	    decklist.remove(0);
+	    //see if the first line is a deck-name. If it is, we use that.
+	    if(first.length() > 1 && first.charAt(0) == '_') {
+		deckName = first.substring(1);
+		decklist.remove(0);
+		continue;
+	    }
+	    
+	    //see if the (new) first line is an identity. If it is, we use that.
+	    if(first.length() > 1 && first.charAt(0) == '#') {
+		idName = first.substring(1);
+		decklist.remove(0);
+		continue;
+	    }
+
+	    //see if we get a faction choice
+	    if(first.length() > 1 && first.charAt(0) == '*') {
+		chosenFaction = first.substring(1);
+		decklist.remove(0);
+		facPassed = true;
+		continue;
+	    }
+
+	    break;
 	}
 
 
@@ -386,9 +497,6 @@ public class Decklists {
 			bin = new TreeMap<String, Integer>();
 			bins.put(type, bin);
 		    }
-
-		    //interface with MWL, if applicable
-		    
 		    bin.put(cardName, numInDeck);
 		} catch (Exception e) {
 		    //do nothing - we just skip this line
@@ -401,6 +509,41 @@ public class Decklists {
 	boolean _first = true;
 	//now we have a list of binned cards: let's print that out
 	//here we also interface with the mwl (if applicable)
+
+	if(chosenFaction == "auto") {
+	    //determine which id gives us the least pips!
+	    Set<String> facList = factions.keySet();
+	    String lowestFac = "anarch";
+	    Integer lowestInf = 99999999;
+	    for(String factionName : facList) {
+		int tally = 0;
+		for(Map.Entry<String, TreeMap<String, Integer>> entry : bins.entrySet()) {
+		    TreeMap<String, Integer> bin = entry.getValue();
+		    for(Map.Entry<String, Integer> entry2 : bin.entrySet()) {
+			Integer num = entry2.getValue();
+			String name = entry2.getKey();
+			String cFaction = cardFaction.get(name);
+			Integer cInfluence = cardInfluence.get(name);
+			if(!cFaction.equals(factionName)) {
+			    if(cInfluence != null) {
+				tally += num*cInfluence;
+			    }
+			}
+		    }
+		}
+
+		if(tally < lowestInf) {
+		    lowestInf = tally;
+		    lowestFac = factionName;
+		}
+	    }
+
+	    System.err.println("%Chose faction " +lowestFac+ " as it had the lowest influence cost (" + lowestInf);
+	    chosenFaction = lowestFac;
+	    facPassed = true;
+	}
+	
+	boolean facWarningDisplayed = false;
 	for(Map.Entry<String, TreeMap<String, Integer>> entry : bins.entrySet()) {
 	    String type = entry.getKey();
 	    TreeMap<String, Integer> bin = entry.getValue();
@@ -408,48 +551,76 @@ public class Decklists {
 	    for(Integer i : bin.values())
 		size += i;
 	    type = type.substring(0, 1).toUpperCase() + type.substring(1);
-
+	    
 	    if(!_first)
 		System.out.println("\\clb");
 	    _first = false;
-
+		
 	    System.out.println(String.format("\\textbf{%s (%d)}\\\\", type, size));
-
+	    
 	    for(Map.Entry<String, Integer> entry2 : bin.entrySet()) {
 		String name = entry2.getKey();
+		Integer num = entry2.getValue();
 		//Entry mwlEntry = chosenMWL.cards.get(cardCode);
-
+		//knowing the name, we can get the faction and influence cost
+		String cFaction = cardFaction.get(name);
+		Integer cInfluence = cardInfluence.get(name);
 		
+		String name2 = name;
+		name = escape(name);
+		//resolve influence
+		if(processFactionInfluence && facPassed) {
+		    if(!chosenFaction.equals(cFaction)) {
+			//only apply influence when the faction is not our faction
+			if(cInfluence != null && cInfluence > 0) {
+			    name = name + " $";
+			    for(int i = 0; i < num * cInfluence; i++) {
+				name = name + "\\circ";
+			    }
+			    name = name + " $";
+			}
+		    }
+		} else if (processFactionInfluence && !facWarningDisplayed) {
+		    System.err.println("% No faction was supplied in the .deck file.  Supply a faction at the top of the file like *weyland-consortium. Alternatively, use *auto to automatically pick the faction with the lowest total inf cost");
+		    facWarningDisplayed = true;
+		}
+	    
 		//restricted: /wp
 		//banned: \times
 		//global_inf: \bullet
+		
+		
 		if(chosenMWL != null) {
 		    for(Entry mwlEntry : chosenMWL.cards.values()) {
-			if(mwlEntry.CARD_NAME.equals(name)) {
+			if(mwlEntry.CARD_NAME.equals(name2)) {
 			    System.err.println("%CARD ON MWL: " + name);
 			    
 			    if (mwlEntry.is_restricted == 1)
-				name = name + "$\\Psi$";
+				name = name + "$ \\Psi$";
 			    else if (mwlEntry.global_penalty > 0) {
 				name = name + " ";
-				for(int i = 0; i < mwlEntry.global_penalty; i++)
+				for(int i = 0; i < mwlEntry.global_penalty * num; i++)
 				    name = name + "$\\bullet$";
 			    }
 			    else if (mwlEntry.universal_faction_cost > 0) {
 				name = name + " ";
-				for(int i = 0; i < mwlEntry.universal_faction_cost; i++)
+				for(int i = 0; i < mwlEntry.universal_faction_cost * num; i++)
 				    name = name + "$\\bullet$";
 			    }
 			    else if(mwlEntry.deck_limit == 0)
 				name = name + " $\\Chi$";
+			    else
+				continue;
+			    break;
 			}
 		    }
 		}
 		
-		Integer num = entry2.getValue();
-		System.out.println(String.format("%d %s\\\\", num, name));
-	    }
-	}
+		
+		System.out.println(String.format("%d %s\\\\", num, name));		
+	    } 
+	} 
+
 	System.out.println("}");
 
 	System.out.println("\\newcommand{\\" + decknamePrefix + ns(deckNum) + "}{\\deckname{" + escape(deckName) + "}}");
